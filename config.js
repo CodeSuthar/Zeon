d5c895fdda089fe05db40a057809bb3b089dacb9
rexxy <112197026+rexxyigso@users.noreply.github.com>
2023-05-12 13:40:41 +0600

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 module.exports = {
     Bot: {
-        Token: process.env.TOKEN || "",
+        Token: process.env.TOKEN || "ODkzNTU5NzAwMTgyOTU0MDQ1.GJMUDe.d3bm_oDdoXgked0fDM8wRvpB_HL3_rHXonzAYI",
         Prefix: "!",
-        NoPrefix: ["990643162928279592"],
-        DeveloperId: ["1060577145258262541"],
+        NoPrefix: ["990643162928279592", "847770840266833961"],
+        DeveloperId: ["1060577145258262541", "847770840266833961"],
         Invite: "https://discord.com/api/oauth2/authorize?client_id=969558840926437406&permissions=1395763178742&scope=bot%20applications.commands"
     },
 
